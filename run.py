ddaefacbf65f14c7f52c8d49195af4ba330b5298
jolodro <andre1112wg@gmail.com>
2025-12-27 19:28:36 -0300

    atualização de inicializador 2

--- run.py
+++ run.py
@@ -1,10 +1,6 @@
 from app import create_app
 
-<<<<<<< HEAD
 app = create_app()
 
 if __name__ == "__main__":
     app.run(debug=True)
-=======
-app.run(debug=False, host="0.0.0.0")
->>>>>>> c9a84f4fa32958c6589e6917a32b2639c0ca45c5
